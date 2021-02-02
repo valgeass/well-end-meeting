6b40a0eb595eabaf1b35a810fc737d6d77f68cfa
valgeass <iwanto.patheexa119@gmail.com>
2021-02-03 00:51:36 +0900

    save name in japanese

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -48,7 +48,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
-  console.log(string);
+  const [isComposed, setIsComposed] = useState(false);
 
   useEffect(() => {
     const disabled = string === '';
@@ -74,11 +74,18 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e)=>setString(e.target.value)}
             onKeyDown={(e)=>{
-              console.log({key: e.key})
+              if(isComposed) return;
+
               if(e.key==='Enter'){
                 setName(e.target.value)
                 e.preventDefault();
               }
+            }}
+            onCompositionStart={() => {
+              setIsComposed(true);
+            }}
+            onCompositionEnd={() => {
+              setIsComposed(false);
             }}
           />
           <Button
