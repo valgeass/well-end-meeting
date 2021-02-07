c28f5f5f186da0d83c07ae30639ba8b370dd1d57
valgeass <iwanto.patheexa119@gmail.com>
2021-02-07 13:33:50 +0900

    apply display new message

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -30,13 +30,16 @@ const MessageList = () => {
     });
     setMessages(newMessages);
     });
-  }, [])
+  }, []);
+
+  const length = messages.length;
   
   return (
     <List className={classes.root}>
       {
-        messages.map(({ key, name, text}) => {
-          return <MessageItem key={key} name={name} text={text} >item</MessageItem>;
+        messages.map(({ key, name, text}, index) => {
+          const isLastItem = length === index + 1;
+          return <MessageItem key={key} name={name} text={text} isLastItem={isLastItem} />;
         })
       }
     </List>
