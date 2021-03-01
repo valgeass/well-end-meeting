550f299963e54568e8daacc49f97dc343131f9de
valgeass <iwanto.patheexa119@gmail.com>
2021-03-01 11:51:19 +0900

    fix pofile

--- src/components/Pages/ProfilePage.js
+++ src/components/Pages/ProfilePage.js
@@ -1,11 +1,11 @@
-import React, { useState, createRef, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { Avatar, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { AppBar } from './AppBar';
 import { gravatarPath } from '../../gravatar';
 import { AuthContext } from '../auth/AuthProvider';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     gridRow: 1,
@@ -17,18 +17,61 @@ const useStyles = makeStyles(() => ({
   title: {
     flexGrow: 1,
   },
+  large: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+  },
+  textStyle: {
+    textAlign: 'left',
+    fontSize: '12px',
+  },
+  profileTextStyle: {
+    textAlign: 'center',
+    fontSize: '32px',
+  },
 }));
 
 const ProfilePage = () => {
   const { profileData } = useContext(AuthContext);
   const classes = useStyles();
-  const ref = createRef();
-  const avatarPath = gravatarPath(profileData.firstName);
+  const avatarPath = gravatarPath(
+    profileData.firstName ? profileData.firstName : 'test'
+  );
 
   return (
     <div>
       <AppBar page="profile" />
-      <Avatar src={avatarPath} />
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <Avatar src={avatarPath} className={classes.large} />
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <Grid container item xs={6}>
+              <Grid item xs={6} className={classes.textStyle}>
+                FirstName
+              </Grid>
+              <Grid item xs={6} className={classes.textStyle}>
+                LastName
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container justify="center">
+            <Grid container item xs={6}>
+              <Grid item xs={6} className={classes.profileTextStyle}>
+                {profileData.firstName}
+              </Grid>
+              <Grid item xs={6} className={classes.profileTextStyle}>
+                {profileData.lastName}
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
