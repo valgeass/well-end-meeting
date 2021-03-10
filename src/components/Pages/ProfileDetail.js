da38141918158238da8d430249517ede51e9f65e
valgeass <iwanto.patheexa119@gmail.com>
2021-03-10 23:35:00 +0900

    commit

--- src/components/Pages/ProfileDetail.js
+++ src/components/Pages/ProfileDetail.js
@@ -1,7 +1,6 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { Avatar, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar } from './AppBar';
 import { gravatarPath } from '../../gravatar';
 import { AuthContext } from '../auth/AuthProvider';
 
